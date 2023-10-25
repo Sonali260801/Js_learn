fe47a536a7178e4a79ab8eccfae81f8ffb343e3e
Sonali260801 <144686245+Sonali260801@users.noreply.github.com>
2023-10-25 10:13:48 +0000

    datatypes summary

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,22 +1,39 @@
-"use strict";// treat all the JS code as newer version
+// "use strict";// treat all the JS code as newer version
 
-// alert(3+3)------we are using node not the browser
-console.log(typeof null)
+// // alert(3+3)------we are using node not the browser
+// console.log(typeof null)
 
 
-// Conversions
-let score="23"
-let score1="23sc"
-console.log(typeof score)
+// // Conversions
+// let score="23"
+// let score1="23sc"
+// console.log(typeof score)
 
-let valueIntNumber=Number(score)
-console.log(typeof valueIntNumber)
+// let valueIntNumber=Number(score)
+// console.log(typeof valueIntNumber)
 
 // but if its alpha numerical it gives NaN value
-let valueIntNumber1=Number(score1)
-console.log(typeof valueIntNumber1)
-console.log(valueIntNumber1)
+// let valueIntNumber1=Number(score1)
+// console.log(typeof valueIntNumber1)
+// console.log(valueIntNumber1)
 
 // Number(score)
 // String(score)
 // Boolean(score)
+
+// ******************** Operations ************************
+// let value=5
+// let negValue=-value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+// console.log(2/2)
+
+// let str1 = "Hello"
+// let str2 = " Sonali"
+// let str3 = str1 + str2
+// console.log(str3);
+
